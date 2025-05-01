c6c9a33aee5a364cc985844ab2886e5264c18b94
AMAN RAJ <raj117557@gmail.com>
2025-05-01 11:30:23 +0530

    port to port

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ connectToDb();
 socketHandler(io);
 
 // Start server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, "0.0.0.0", () =>
+const port = process.env.PORT || 5000;
+server.listen(port, "0.0.0.0", () =>
   console.log(`Server running on port ${PORT}`)
 );
