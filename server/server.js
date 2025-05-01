cf12357569d0505d4b26488b95271237d1f0bdde
AMAN RAJ <raj117557@gmail.com>
2025-05-01 08:08:30 +0530

    fghj

--- server/server.js
+++ server/server.js
@@ -48,5 +48,7 @@ connectToDb();
 socketHandler(io);
 
 // Start server
-const PORT = process.env.PORT || 8181;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on port ${PORT}`)
+);
