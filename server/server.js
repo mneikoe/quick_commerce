065595395e3d55c7225ecf186443eb1224cda368
AMAN RAJ <raj117557@gmail.com>
2025-05-02 12:27:04 +0530

    ertghhgh

--- server/server.js
+++ server/server.js
@@ -12,22 +12,17 @@ const __dirname = dirname(__filename);
 
 // Rest of your imports...
 import { connectToDb } from "./config/db.js";
-import socketHandler from "./sockets/socketHandler.js";
+
 import router from "./routes/index.js";
 import errorHandler from "./middlewares/errorMiddleware.js";
 import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: process.env.FRONTEND_URL } });
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
 
 // CORS middleware
 app.use(cors());
@@ -45,7 +40,6 @@ app.use(errorHandler);
 
 // Database & Socket
 connectToDb();
-socketHandler(io);
 
 // Start server
 const port = process.env.PORT || 5000;
